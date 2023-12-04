772143d99cf5fa5c86a3c90646c5f577e4f976f9
Nigel Jeche <jechekudzie@gmail.com>
2023-12-04 10:51:14 +0200

    Initial Commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('administration.index');
+    }
+
+    public function table()
+    {
+        return view('administration.table');
+    }
+
+    public function form()
+    {
+        return view('administration.form');
+    }
+
+    public function dashboard()
+    {
+        return view('administration.dashboard');
+    }
+    public function pickers()
+    {
+        return view('administration.pickers');
+    }
+}
